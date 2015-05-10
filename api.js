25b4113c6c5260fe1176dda549b794ce59969baa
SlashMan <andzhilin@gmail.com>
2015-05-11 00:42:44 +0400

    Beta

---
+++ api.js
@@ -0,0 +1,111 @@
+/**
+ * Created by SlashMan on 10.05.2015.
+ */
+var express = require('express');
+var app = express();
+var md5 = require('MD5');
+var Sequelize = require('sequelize');
+
+
+var sequelize = new Sequelize('license', 'root', '884088',{
+    'host' : 'localhost'
+    //,'logging': false
+});
+
+
+var License = sequelize.define('License', {
+    id: {type: Sequelize.INTEGER, primaryKey: true },
+    user: Sequelize.TEXT,
+    code: Sequelize.TEXT,
+    one_ip: Sequelize.INTEGER
+},{
+    timestamps: false,
+    tableName: 'license'
+});
+
+var Launch = sequelize.define('Launch', {
+    id: {type: Sequelize.INTEGER, primaryKey: true },
+    occured: Sequelize.INTEGER,
+    ip: Sequelize.STRING(45),
+    machine_hash: Sequelize.STRING(45),
+    license_id: {
+        type: Sequelize.INTEGER,
+        references: License,
+        referencesKey: "id"
+    }
+},{
+    timestamps: false,
+    tableName: 'launch'
+});
+
+License.hasOne(Launch, {foreignKey: 'license_id', timestamps: false});
+
+app.get('/api/launch/:code/:machineHash',function(req,res){
+    if(typeof(req.params["code"]) !== 'undefined' && typeof(req.params["machineHash"]) !== 'undefined')
+    {
+        var code = req.params["code"];
+        var remoteIp = req.ip;
+        var currentMachineHash = req.params["machineHash"];
+        var currentTimestamp = Math.floor(Date.now() / 1000);
+
+        License.findAll({
+            where: {
+                code: code
+            }
+        }).then(function(licenses){
+            if(licenses.length == 1)
+            {
+                var license = licenses[0];
+
+                Launch.find({
+                    where: {
+                        license_id: license.dataValues['id']
+                    }
+                }).then(function(dbLaunch){
+                    if(dbLaunch == null)
+                    {
+                        Launch.create(
+                            {
+                                ip: remoteIp,
+                                occured: currentTimestamp,
+                                machineHash: currentMachineHash,
+                                license_id: license.dataValues['id']
+                            }
+                        );
+                        res.json({"error_code": 2, "status": "success"});
+                    }
+                    else {
+                        var lastIp = dbLaunch.dataValues["ip"];
+                        var lastTime = parseInt(dbLaunch.dataValues["occured"]);
+                        var machineHash = dbLaunch.dataValues["machine_hash"];
+
+                        if((lastIp != remoteIp || machineHash != currentMachineHash) && lastTime < (currentTimestamp - 300) && code != 'test') //@todo Recheck
+                        {
+                            res.json({"error": "Launched from another computer!", "error_code": 3, "status": "error"});
+                        }
+                        else {
+                            dbLaunch.machine_hash = currentMachineHash;
+                            dbLaunch.ip = remoteIp;
+                            dbLaunch.occured = currentTimestamp;
+                            dbLaunch.save();
+
+                            res.json({"error_code": 2, "status": "success"});
+                        }
+                    }
+                });
+            }
+            else {
+                res.json({"error": "Wrong license", "error_code": 1, "status": "error"});
+            }
+        });
+    }
+});
+
+app.get('/api/ip', function(req,res){
+    res.json({ip: req.ip});
+});
+
+/*Run the server.*/
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
